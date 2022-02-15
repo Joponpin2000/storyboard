111aee9db574c5c986d26337d3c28cc4e743ea87
Dev Jo <idowuayanfeoluwa@gmail.com>
2022-02-15 21:02:46 +0100

    feat: header nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import PageTabs from "../components/molecules/PageTabs";
+import HeaderNav from "../components/organisms/HeaderNav";
+import PageSection from "../components/organisms/PageSection";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <div>
+      <HeaderNav />
+      <PageSection />
+      <PageTabs />
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
