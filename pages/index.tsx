15c309aa27569905afb517b2e98a13d068e603a3
Dev Jo <idowuayanfeoluwa@gmail.com>
2022-02-16 14:24:28 +0100

    feat: added font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import HeaderNav from "../components/organisms/HeaderNavbar";
 import PageSection from "../components/organisms/PageSection";
 
 const IndexPage = () => {
   return (
     <div className="Dashboard">
+      <link
+        rel="preload"
+        href="/fonts/fellix/fellix-Regular.cssf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/fellix/fellix-Medium.css"
+        as="font"
+        crossOrigin=""
+      />
       <HeaderNav />
       <PageSection />
     </div>
